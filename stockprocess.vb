f7943ee41e4522313ed6e197e074550ff07a7f3c
kehull <71107280+kehull@users.noreply.github.com>
2020-11-13 22:48:30 -0600

    Add files via upload
    This assignment was quite a challenge! I ended up talking to my tutor to figure out why my code wouldn't loop through all the changes- it turns out it was a scope issue, and I just needed to set a worksheet variable and deploy it on my Cells, Range, and Columns properties to get the code to iterate through all worksheets, even though the rest of the For Each loop to do so was already in place. In all honesty, I don't understand why the worksheets variable has to be attached to these properties, but I do understand that it needs to be there. In my research, I stumbled over the concept of Offset and CurrentRegion properties, which I wish I'd known before I started writing the code as it would have prevented some of my simple formatting errors early on in the project.
    
    Perhaps the biggest idiosyncrasy of this project is found in lines 53-55. I know there's a way to retrieve the value I was seeking by using the If Cells(x,y) <> Cells(x,y+1) sort of thing, but for the life of me i couldn't get it to work on the first line of code in the project. When I tried to use my Google skills to find a solution, I stumbled over a few sites where former students of this class had uploaded the entire assignment to a help site and asked for input. I stayed clear of these pages, but had a hard time finding a solution anywhere else, so I rubbed all six of my brain cells together until I came up with the concept of the "nope"/"yup" checker. I'm certain I'm neither the first nor last person to use this method, so I make no claims of discovery, but I'm proud of myself for coming to that solution on my own.
    
    Another topic I discussed with my tutor was how to use GitHub at all, as this was my first experience doing so. Since I didn't talk to him until my homework was basically finished, I only ended up doing two commits for this project. In the future, I'll try to do more commits on my projects.

--- stockprocess.vb
+++ stockprocess.vb
@@ -1,78 +1,128 @@
-
-
 Sub stockprocess()
 
-' Setup
-Cells(1, 9).Value = "Ticker"
-Cells(1, 10).Value = "Yearly Change"
-Cells(1, 11).Value = "Percent Change"
-Cells(1, 12).Value = "Total Stock Volume"
-Cells(2, 15).Value = "Greatest % Increase"
-Cells(3, 15).Value = "Greatest % Decrease"
-Cells(4, 15).Value = "Greatest Total Volume"
-Cells(1, 19).Value = "Ticker"
-Cells(1, 20).Value = "Value"
-Columns("A:T").AutoFit
+'Complete this process on each worksheet in the dataset
+Dim WS_Count As Integer
+Dim x As Integer
+WS_Count = ActiveWorkbook.Worksheets.Count
+Dim ws As Worksheet
 
+For Each ws In Worksheets
 
-' Print Ticker and Total Stock Volume to Summary Table
+' Begin the loop.
+For x = 1 To WS_Count
 
 ' Variables
 Dim Ticker As String
 Dim Stock_Total As Double
-Stock_Total = 0
+    Stock_Total = 0
 Dim Stummary_Table_Row As Integer
-Summary_Table_Row = 2
+    Summary_Table_Row = 2
 Dim LastRow As Long
-LastRow = Cells(Rows.Count, 1).End(xlUp).Row
-
-
+    LastRow = ws.Cells(Rows.Count, 1).End(xlUp).Row
+Dim Opening_Price_Checker As String
+    Opening_Price_Checker = "nope"
+    
 ' Loop through stocks
-For i = 2 To LastRow
+For I = 2 To LastRow
+    ' Determine if next ticker value matches the previous ticker value or not
+If ws.Cells(I + 1, 1).Value <> ws.Cells(I, 1).Value Then
+    'If ticker does not match:
+    ' Set ticker value to new ticker
+    Ticker = ws.Cells(I, 1).Value
+    ' Add to stock total volume
+    Stock_Total = Stock_Total + ws.Cells(I, 7).Value
+    ' Print ticker to summary table
+    ws.Range("I" & Summary_Table_Row).Value = Ticker
+    ' Print the stock total volume to summary table
+    ws.Range("L" & Summary_Table_Row).Value = Stock_Total
+    ' Reset the stock total
+    Stock_Total = 0
+    ' Add one to the summary table row
+    Summary_Table_Row = Summary_Table_Row + 1
 
-' Determine if next ticker matches the previous ticker or not
-    If Cells(i + 1, 1).Value <> Cells(i, 1).Value Then
-    ' If ticker does not match:
-        ' Set ticker value to new ticker
-            Ticker = Cells(i, 1).Value
-        ' Add to stock total volume
-            Stock_Total = Stock_Total + Cells(i, 7).Value
-        ' Print ticker to summary table
-            Range("I" & Summary_Table_Row).Value = Ticker
-      ' Print the stock total volume to summary table
-            Range("L" & Summary_Table_Row).Value = Stock_Total
-      ' Add one to the summary table row
-            Summary_Table_Row = Summary_Table_Row + 1
-      ' Reset the stock total
-            Stock_Total = 0
-      ' Set first opening price of a ticker as a variable
-            Dim Opening_Price As Double
-            Opening_Price = Cells(i + 1, 3).Value
 
     ' If the ticker does match:
     Else
-
-      ' Add to the stock total
-            Stock_Total = Stock_Total + Cells(i, 3).Value
-      ' Set closing price of ticker as a variable- this version will keep setting every new closing price
-      ' as the last price and will stop updating the value when the ticker cells no longer match
-            Dim Closing_Price As Double
-            Closing_Price = Cells(i, 6).Value
-        ' Set variable so i don't have to type out this reference cell a million times
-        Dim Yearly_Change As Range
-        Set Yearly_Change = Cells(Summary_Table_Row, 10)
-        ' Calculate yearly change
-            Yearly_Change.Value = (Closing_Price - Opening_Price)
-            ' Conditional formatting turns cell green if value is positive, red if negative
-                If Yearly_Change.Value < 0 Then
-                    Yearly_Change.Interior.Color = vbRed
-                Else
-                    Yearly_Change.Interior.Color = vbGreen
-                End If
-            ' Calculate percent change
-                
-                'Cells(Summary_Table_Row, 11).Value = ((Yearly_Change.Value / 4) * 100)
+        ' Set first opening price of a ticker as a variable
+        ' The first time this code runs on a sheet, Opening_Price_Checker
+        ' already = "nope". Because of this, the code will pull the first
+        ' value in the row where the loop starts. This section then changes
+        ' Opening_Price_Checker to "yup", which causes the statement to evaluate
+        ' as false in every subsequent iteration, preventing it from pulling
+        ' any more values from the opening price column.
+        If Opening_Price_Checker = "nope" Then
+            Opening_Price_Checker = "yup"
+            Opening_Price = ws.Cells(I, 3).Value
+        Else
+        End If
+        'Set last closing price of a ticker as a variable
+        ' Because Opening_Price_Checker now = "yup", after retrieving
+        ' the first opening price, the code exits that part of the statement
+        ' and enters this section, which checks for the final appearance of
+        ' a particular ticker value and then retrieves the closing price
+        ' value from its row.
+        If ws.Cells(I + 1, 6).Value <> ws.Cells(I, 6).Value Then
+            Closing_Price = ws.Cells(I + 1, 6).Value
+        Else
+        End If
+        ' Because Opening_Price_Checker has global scope, once the code has
+        ' run all the way through and starts the next iteration, Opening_Price_Checker
+        ' will = "nope" again, allowing the code to retrieve the opening price
+        ' for each ticker.
+   
+    
+    ' Add to the stock total
+    Stock_Total = Stock_Total + ws.Cells(I, 7).Value
+    ' Set variable so i don't have to type out this reference cell a million times
+    Dim Yearly_Change As Double
+    ' Calculate yearly change
+    Yearly_Change = Closing_Price - Opening_Price
+    
+    ws.Cells(Summary_Table_Row, 10).Value = Yearly_Change
+    ' Conditional formatting turns cell green if value is positive, red if negative
+        If Yearly_Change < 0 Then
+            ws.Cells(Summary_Table_Row, 10).Interior.Color = vbRed
+        Else
+            ws.Cells(Summary_Table_Row, 10).Interior.Color = vbGreen
+        End If
+    ' Calculate percent change
+    ws.Cells(Summary_Table_Row, 11).Value = (Yearly_Change / Opening_Price)
+    ws.Cells(Summary_Table_Row, 11).NumberFormat = "#,##0.00" + "%"
     End If
-Next i
-End Sub
+Next I
 
+' set variables for finding the greatest increase, greatest decrease, and greatest total volume
+' use the min/max functions to solve each variable
+' print the solved variables to the appropriate cells
+Dim Greatest_Increase As Double
+    Greatest_Increase = Application.WorksheetFunction.Max(ws.Columns("J"))
+    ws.Range("Q2").Value = Greatest_Increase
+Dim Greatest_Decrease As Double
+    Greatest_Decrease = Application.WorksheetFunction.Min(ws.Columns("J"))
+    ws.Range("Q3").Value = Greatest_Decrease
+Dim Greatest_Total_Volume As Double
+    Greatest_Total_Volume = Application.WorksheetFunction.Max(ws.Columns("L"))
+    ws.Range("Q4").Value = Greatest_Total_Volume
+    
+' Set up additional cells.
+' Because the data is laid out consistantly across all sheets, we can assume
+' the exact coordinates of these cells will be the same on every sheet.
+' This is not alway the case in every project, but I'm taking advantage
+' of it here.
+ws.Cells(1, 9).Value = "Ticker"
+ws.Cells(1, 10).Value = "Yearly Change"
+ws.Cells(1, 11).Value = "Percent Change"
+ws.Cells(1, 12).Value = "Total Stock Volume"
+ws.Cells(2, 15).Value = "Greatest % Increase"
+ws.Cells(3, 15).Value = "Greatest % Decrease"
+ws.Cells(4, 15).Value = "Greatest Total Volume"
+ws.Cells(1, 16).Value = "Ticker"
+ws.Cells(1, 17).Value = "Value"
+
+' this part isn't necessary to the assignment, it just makes the tables easier to look at
+ws.Columns("A:T").AutoFit
+
+' Go to the next worksheet
+Next x
+Next ws
+End Sub
